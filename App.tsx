6cfbab44bf8f23f7061daf25d0538ea225de50e8
unknown <attilahegyeli@gmail.com>
2025-06-30 11:28:21 +0300

    Atomic design model

--- App.tsx
+++ App.tsx
@@ -7,17 +7,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <text>Changes you make will automatically reload.</text>
-      <Text>Shake your phone to open the developer menu.</Text>
-      <Text>Welcome to React Native!</Text>
-      <Text>Welcome to Expo!</Text>
+    
+
+    
+    
       <StatusBar style="auto" />
 
 
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
+      
     </View>
   );
 }

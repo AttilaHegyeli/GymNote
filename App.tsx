a5eb47a430a4f43ca6f6024e9627f69ccee5caa3
unknown <attilahegyeli@gmail.com>
2025-07-02 10:29:55 +0300

    Button atom

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 import CustomText from './src/components/atoms/Text/text';
+import CustomButton from './src/components/atoms/Button/button';
 import React from 'react';
 
 
@@ -9,7 +10,10 @@ export default function App() {
     <View style={styles.container}>
       
       <CustomText message="Hello, this is a custom text component!" />
-
+      <CustomButton 
+        title="Press Me" 
+        onPress={() => Alert.alert('Button Pressed!')}
+      />
     
       <StatusBar style="auto" />
     </View>
